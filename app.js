bebcf5c52e14ff56b09840d9ffa918b86d136b85
TJ Li <TianjianLi314@gmail.com>
2023-08-08 11:10:18 -0500

    Added env variable

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const mongoDb = "YOUR MONGO URL HERE";
+const mongoDb = process.env.MONGO_STRING;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
